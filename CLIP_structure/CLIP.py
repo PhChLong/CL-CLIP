86fc76a5ca52f08ea338230e29e1f9af9ee6a349
LongPC1826 <long.pc2416714@sis.hust.edu.vn>
2026-03-19 21:29:54 +0700

    19/3- build CLIP structure v1.0

---
+++ CLIP_structure/CLIP.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class CLIPAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+class CLIPTextTransformer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+class CLIPVisionTransformer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+class CLIP(nn.Module):
+    def __init__(self):
+        super().__init__()
+
